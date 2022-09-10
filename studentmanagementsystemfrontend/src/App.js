e07fd84b93a2db6d67633d3504bfff89e60796c4
Hemil321 <hkevadiya1104@gmail.com>
2022-09-10 18:56:25 +0530

    Added Footer

--- studentmanagementsystemfrontend/src/App.js
+++ studentmanagementsystemfrontend/src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Student from './components/Student';
-import StudentList from './components/StudentList';
-import NavigationBar from './components/NavigationBar';
+import Student from './Components/Student.js';
+import StudentList from './Components/StudentList.js';
+import NavigationBar from './Components/NavigationBar.js';
 
 function App() {
   return (
